25a331e1bd00e098ad3028ee9c9075af570911c0
Dan Dees <dand@appsmiths.com>
2026-01-03 13:50:27 +0700

    Fixed: UBSAN strict aliasing violation in xdr_enum serialization
    Replaced non-template xdr_enum(xdr_t*, xdr_op*) with a type-safe
    template that accepts any enum type. This eliminates the need for
    reinterpret_cast<xdr_op*> at call sites which violates strict aliasing
    rules and triggers undefined behavior under UBSAN.

--- src/remote/protocol.cpp
+++ src/remote/protocol.cpp
@@ -292,7 +292,7 @@ bool_t xdr_protocol(RemoteXdr* xdrs, PACKET* p)
 
 	DEBUG_XDR_PACKET(xdrs, p);
 
-	if (!xdr_enum(xdrs, reinterpret_cast<xdr_op*>(&p->p_operation)))
+	if (!xdr_enum(xdrs, &p->p_operation))
 		return P_FALSE(xdrs, p);
 
 #if COMPRESS_DEBUG > 1
@@ -320,9 +320,9 @@ bool_t xdr_protocol(RemoteXdr* xdrs, PACKET* p)
 	case op_connect:
 		{
 			P_CNCT* connect = &p->p_cnct;
-			MAP(xdr_enum, reinterpret_cast<xdr_op&>(connect->p_cnct_operation));
+			MAP(xdr_enum, connect->p_cnct_operation);
 			MAP(xdr_short, reinterpret_cast<SSHORT&>(connect->p_cnct_cversion));
-			MAP(xdr_enum, reinterpret_cast<xdr_op&>(connect->p_cnct_client));
+			MAP(xdr_enum, connect->p_cnct_client);
 			MAP(xdr_cstring_const, connect->p_cnct_file);
 			MAP(xdr_short, reinterpret_cast<SSHORT&>(connect->p_cnct_count));
 
@@ -340,7 +340,7 @@ bool_t xdr_protocol(RemoteXdr* xdrs, PACKET* p)
 				}
 
 				MAP(xdr_short, reinterpret_cast<SSHORT&>(tail->p_cnct_version));
-				MAP(xdr_enum, reinterpret_cast<xdr_op&>(tail->p_cnct_architecture));
+				MAP(xdr_enum, tail->p_cnct_architecture);
 				MAP(xdr_u_short, tail->p_cnct_min_type);
 				MAP(xdr_u_short, tail->p_cnct_max_type);
 				MAP(xdr_short, reinterpret_cast<SSHORT&>(tail->p_cnct_weight));
@@ -359,7 +359,7 @@ bool_t xdr_protocol(RemoteXdr* xdrs, PACKET* p)
 	case op_accept:
 		accept = &p->p_acpt;
 		MAP(xdr_short, reinterpret_cast<SSHORT&>(accept->p_acpt_version));
-		MAP(xdr_enum, reinterpret_cast<xdr_op&>(accept->p_acpt_architecture));
+		MAP(xdr_enum, accept->p_acpt_architecture);
 		MAP(xdr_u_short, accept->p_acpt_type);
 		DEBUG_PRINTSIZE(xdrs, p->p_operation);
 		return P_TRUE(xdrs, p);
@@ -368,7 +368,7 @@ bool_t xdr_protocol(RemoteXdr* xdrs, PACKET* p)
 	case op_cond_accept:
 		accept_with_data = &p->p_acpd;
 		MAP(xdr_short, reinterpret_cast<SSHORT&>(accept_with_data->p_acpt_version));
-		MAP(xdr_enum, reinterpret_cast<xdr_op&>(accept_with_data->p_acpt_architecture));
+		MAP(xdr_enum, accept_with_data->p_acpt_architecture);
 		MAP(xdr_u_short, accept_with_data->p_acpt_type);
 		MAP(xdr_cstring, accept_with_data->p_acpt_data);
 		MAP(xdr_cstring, accept_with_data->p_acpt_plugin);
