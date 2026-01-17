0d33bb4f6d0426e18f52d4b20bad0528bb05e3ff
Dan Dees <dand@appsmiths.com>
2026-01-17 20:13:02 +0700

    rm-pkg.sh

--- rm-pkg.sh
+++ rm-pkg.sh
@@ -1,2 +1,10 @@
-declare -r pkg='firebird5'
-rm -rfv ${ASV_AF_PORTS}/build/${pkg}-build-${aplatform} ${ASV_PLAT_PORTS}/${pkg}
+#!/usr/bin/env bash
+
+# fail on error
+set -eo pipefail
+
+# get the repo name from the directory basename where the script is located
+declare -r pkg="$(basename "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
+
+# delete
+rm -rvf ${ASV_AF_PORTS}/build/${pkg}-build-${aplatform} ${ASV_PLAT_PORTS}/${pkg}
